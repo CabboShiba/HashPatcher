ef0096684e966c76b89b5e5e2cf87c3d688bcf47
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2022-09-15 19:24:38 +0200

    Add files via upload

---
+++ HashPatcher/Utils/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPatcher
+{
+    internal class Utils
+    {
+        private static Random random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789qwertyuioplkjhgfdsazxcvbnm";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
